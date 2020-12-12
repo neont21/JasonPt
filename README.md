7097b8bb659e192c45cc0efa7138cb1a4457cc86
Pt J <peter.j@kakao.com>
2020-12-12 11:50:08 +0900

    Add restrict-user mode

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Jason Bot JasonPt#2964
 ### Environment Variables
 - `DISCORD_TOKEN` : Bot API Token
 - `DISCORD_BOT_ID` : Bot ID for the mention (prefix)
+- `DISCORD_USER_ID` : User ID for `only_user` (option for `RESTRICT_USER` mode)
 
 ### Features
 - PREFIX: Bot Mention
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::env;
 // for Discord API
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
+    model::{channel::Message, gateway::Ready, id::UserId},
     prelude::*,
 };
 
 struct Handler;
+
+static RESTRICT_USER: bool = true;
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -21,8 +23,10 @@ impl EventHandler for Handler {
         // send `Pong!` by `ping`
         // if it's fail, print error message.
         if msg.content == format!("<@!{}> ping", bot_id) {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
+            if RESTRICT_USER {
+                only_user("Pong!", ctx, msg).await;
+            } else {
+                all_user("Pong!", ctx, msg).await;
             }
         }
     }
@@ -31,6 +35,28 @@ impl EventHandler for Handler {
     // It prints the name of the bot on the console.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+    }
+}
+
+async fn all_user(s: &str, ctx: Context, msg: Message) {
+    if let Err(why) = msg.channel_id.say(&ctx.http, s).await {
+        println!("Error sending message: {:?}", why);
+    }
+}
+    
+async fn only_user(s: &str, ctx: Context, msg: Message) {
+    let user_id = env::var("DISCORD_USER_ID")
+        .expect("Expected a user id in the environment");
+    let user_id: u64 = user_id.parse().unwrap();
+
+    if msg.author.id == UserId(user_id) {
+        if let Err(why) = msg.channel_id.say(&ctx.http, s).await {
+            println!("Error sending message: {:?}", why);
+        }
+    } else {
+        if let Err(why) = msg.channel_id.say(&ctx.http, format!("난 <@!{}> 말만 들을건데요", user_id)).await {
+            println!("Error sending message: {:?}", why);
+        }
     }
 }
 
